fa8321c2c37d3755c090c54b2430837e7363e3ef
252505Sonali@ <praveshkumar1062002@gmail.com>
2022-08-24 01:17:33 +0530

    All commits

---
+++ src/components/Footer.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Footer(props) {
+  return (
+    <div className="footer">
+      <p>Ⓒ Programmig with Pravesh</p>
+    </div>
+  );
+}
+
+export default Footer;
